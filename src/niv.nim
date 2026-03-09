3afb18d588a737467016a969e4c0923e48c3b825
akvilary <akvilary@gmail.com>
2026-03-09 19:43:52 +0800

    feat: open file explorer when launching with a directory argument

--- src/niv.nim
+++ src/niv.nim
@@ -2,11 +2,20 @@
 
 import std/os
 import niv/editor
+import niv/types
 
 proc main() =
   let args = commandLineParams()
-  let filePath = if args.len > 0: args[0] else: ""
-  var state = newEditorState(filePath)
+  let arg = if args.len > 0: args[0] else: ""
+  var state: EditorState
+  if arg.len > 0 and dirExists(arg):
+    setCurrentDir(arg)
+    state = newEditorState("")
+    state.sidebar.visible = true
+    state.sidebar.focused = true
+    state.mode = mExplore
+  else:
+    state = newEditorState(arg)
   state.run()
 
 when isMainModule:
